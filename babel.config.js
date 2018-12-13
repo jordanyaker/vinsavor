568773d26008f8cf66cf81784ef01bfb29a366c4
Jordan Yaker <jordan.e.yaker@gmail.com>
2018-12-13 09:17:02 -0500

    Updated the babel setup to work without requiring multiple environments.

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "presets": [
+        "@babel/preset-react",
+        [
+            "@babel/preset-env",
+            {
+                "useBuiltIns": "entry"
+            }
+        ]
+    ],
+    "plugins": [
+        "@babel/plugin-proposal-class-properties"
+    ]
+}
