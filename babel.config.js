c9b4636d0c7ade21dd446979e4fb727e204bc5a3
Jordan Yaker <jordan.e.yaker@gmail.com>
2018-12-17 13:41:36 -0500

    Added the missing modules and updated the plugin loader references.

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,18 @@
 module.exports = {
     "presets": [
-        "@babel/preset-react",
+        "@babel/react",
         [
-            "@babel/preset-env",
+            "@babel/env",
             {
                 "useBuiltIns": "entry"
             }
         ]
     ],
     "plugins": [
-        "@babel/plugin-proposal-class-properties"
+        "@babel/plugin-proposal-class-properties",
+        "@babel/plugin-proposal-export-default-from",
+        "@babel/plugin-proposal-export-namespace-from",
+        "@babel/plugin-proposal-object-rest-spread",
+        "@babel/plugin-syntax-dynamic-import",
     ]
 }
